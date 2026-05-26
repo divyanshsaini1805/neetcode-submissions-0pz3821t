95d63af9672d6bbe6558d48f1bc34ff4daef180f
Divyansh Saini <divyanshsaini1805@gmail.com>
2026-05-26 19:27:49 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true; // empty string
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                // if prefix is valid and substring exists in dictionary
+                if (dp[j] && words.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
